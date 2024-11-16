d3801aa48e6f00159abc13ee438a109682e00a3c
NabinPaudel001 <paudelnabin41@gmail.com>
2024-11-16 20:10:02 +0545

    test edit

--- website/src/app/page.js
+++ website/src/app/page.js
@@ -4,7 +4,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="justify-center">
-      <h1>Hello Mic testing Mic testing 1...2..3.</h1>
+       <h1 className="text-3xl font-bold">This is a demon trying to meditate for super duper powers</h1>
+      <img
+              src="demon.jpeg"
+              alt="Demon Featured"
+              className="w-50 h-50"
+            />
     </div>
   );
 }
