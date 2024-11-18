f13a5ca04c512a1bbb91b5b2ee79e91624c7d7b6
RushabRisal <rushabofficials@gmail.com>
2024-11-18 22:55:02 +0545

    helele

--- website/src/components/footer/Footer.js
+++ website/src/components/footer/Footer.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link';
 function Footer() {
   return (
-    <div className="bg-[#231f20] text-white h-full w-full flex flex-wrap justify-between items-center tracking-wider poppins-extralight text-lg px-6 md:px-28">
+    <div className="bg-[#231f20] text-white h-full w-full flex flex-col md:flex-row flex-wrap justify-between items-center tracking-wider poppins-extralight text-lg px-6 md:px-28 py-7 md:py-0">
       <div>
         <Image
           src={'/hult_image/hultprize_logo_light.png'}
@@ -16,9 +16,9 @@ function Footer() {
         <div  className='poppins-thin text-sm tracking-wider leading-[30px]'>
           <h1 className='poppins-semibold text-[27px]'>Quick Links</h1>
           <Link href='/'>Home</Link>
-          <p>About</p>
-          <p>Events</p>
-          <Link href='/Contact'>Contact</Link>
+          <p >About</p>
+          <p >Events</p>
+          <Link href='/Contact' >Contact</Link>
         </div>
         <div className='poppins-thin text-sm tracking-wider leading-[30px]'>
           <h1 className='poppins-semibold text-[27px]'>Contact Us</h1>
