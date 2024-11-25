665d16d041fb14911ae77087181ac53024340b2d
IntelligentBeaver <aman.211506@ncit.edu.np>
2024-11-25 22:55:36 +0545

    Event Card Implemented - Mother father.... all it took was complete reclone and npm install to work.

--- website/src/components/common/EventSection.js
+++ website/src/components/common/EventSection.js
@@ -1,4 +1,9 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import Button from "./Button";
 
 const EventCard = ({ event, hover, transition }) => (
@@ -11,7 +16,7 @@ const EventCard = ({ event, hover, transition }) => (
     </div>
     <div className="card-content max-w-3xl flex flex-col items-center justify-between gap-6 px-4 py-4 rounded-b-2xl bg-blackMain">
       <p className="">{event.info}</p>
-      <div className="flex-auto ">
+      <div className="">
         <Button />
       </div>
     </div>
@@ -32,12 +37,14 @@ const EventSection = () => {
       Current Event Section
 
       */}
-      <div className="current-section flex flex-col gap-12">
-        <h4 className="font-extrabold text-nowrap">Happening Now:</h4>
+      <div className="current-section flex flex-col gap-8">
+        <h4 className="font-extrabold text-nowrap text-accent">
+          Happening Now:
+        </h4>
         {currentEvent ? (
           <EventCard
             event={currentEvent}
-            hover="scale-110"
+            hover="scale-100"
             transition="transition-all duration-300 ease-in-out"
           />
         ) : (
@@ -50,23 +57,39 @@ const EventSection = () => {
       Events Section
 
        */}
-      <div className="upcoming-section flex flex-col gap-12">
+      <div className="upcoming-section flex flex-col gap-8">
         <div>
           <h4 className="font-extrabold">Coming Later:</h4>
         </div>
-        <div className="card-stack flex gap-8">
-          {upcomingEvents.length > 0 ? (
-            upcomingEvents.map((event, index) => (
-              <EventCard
-                key={index}
-                event={event}
-                hover="hover:scale-110"
-                transition="transition-all duration-300 ease-in-out"
-              />
-            ))
-          ) : (
-            <p>No upcoming events</p>
-          )}
+        <div className=" w-[900px]">
+          <Swiper
+            className="swiper-container scal"
+            slidesPerView={3}
+            freeMode={true}
+            spaceBetween={-40}
+            autoHeight={true}
+            navigation
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Navigation, Pagination]}
+          >
+            {upcomingEvents.length > 0 ? (
+              upcomingEvents.map((event, index) => (
+                <SwiperSlide key={index} className="rounded-lg">
+                  {/* Optional for styling */}
+                  <EventCard
+                    key={index}
+                    event={event}
+                    hover="scale-75 hover:scale-90"
+                    transition="transition-all duration-300 ease-in-out"
+                  />
+                </SwiperSlide>
+              ))
+            ) : (
+              <p>No upcoming events</p>
+            )}
+          </Swiper>
         </div>
       </div>
     </div>
