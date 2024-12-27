a12a403e988cf38343eb338cc5e53add80aed378
RushabRisal <rushabofficials@gmail.com>
2024-12-27 15:50:18 +0545

    fixed the button animation

--- website/src/components/common/RegAuth.js
+++ website/src/components/common/RegAuth.js
@@ -8,7 +8,7 @@ function RegAuth({link}) {
       onClick={() => {
         console.log("clicked");
       }}
-      className="button-anim font-medium bg-shiny text-[12px] px-3 py-[4px] bg-gradient-to-r from-[#eb008b] via-[#f37bc1cc] to-[#eb008b] sm:px-4 sm:py-[4px] sm:text-[20px] text-nowrap  hover:text-white  hover:outline animate-shiny  hover:outline-[0.15px] rounded-[30px] text-white"
+      className="button-anim font-medium bg-shiny text-[12px] px-3 py-[4px] bg-gradient-to-r from-[#eb008b] via-[#e969b4d2] to-[#eb008b] sm:px-4 sm:py-[4px] sm:text-[20px] text-nowrap  hover:text-white  hover:outline animate-shiny  hover:outline-[0.15px] rounded-[30px] text-white"
     >
       <a href={link} target="_blank">Register Now</a>
     </button>
